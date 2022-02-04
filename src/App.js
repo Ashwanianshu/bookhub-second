f690ab4eac6f01f5d570b7be9d4ea5791df4e8e6
Ashwani Singh Rajpoot <ashwanisinghrajpoot@Ashwanis-MacBook-Air.local>
2022-02-04 21:43:22 +0530

    commit message

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import {Component} from 'react'
+import './App.css'
+import {Route, Switch} from 'react-router-dom'
+import Home from './components/Home '
+import BookDetails from './components/BookDetails'
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import Bookshelves from './components/BookShelves'
+import BookContext from './Context/bookContext'
+import NotFound from './components/NotFound'
+// use the below bookshelvesList for rendering read status of book items in Bookshelves Route
+
+class App extends Component {
+  state = {
+    activeRoute: 'home',
+  }
+
+  alterActiveRoute = activeRouteValue => {
+    this.setState(
+      {
+        activeRoute: activeRouteValue,
+      },
+      this.setTheLocalStorage,
+    )
+  }
+
+  setTheLocalStorage = () => {
+    const {activeRoute} = this.state
+    localStorage.setItem('activeLink', activeRoute)
+  }
+
+  render() {
+    const {activeRoute} = this.state
+    return (
+      <BookContext.Provider
+        value={{
+          activeRoute,
+          alterActiveRoute: this.alterActiveRoute,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/shelf" component={Bookshelves} />
+          <ProtectedRoute exact path="/books/:id" component={BookDetails} />
+          <NotFound />
+        </Switch>
+      </BookContext.Provider>
+    )
+  }
+}
+
+export default App
